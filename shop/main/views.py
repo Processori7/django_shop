3f439257f51825219082cf6ea903b4f1431548a0
Igor Usenko <usenkoigor01@gmail.com>
2025-07-14 13:10:33 +0300

    Закончил приложение main, разобрался с URL, сделал приложение самостоятельным, подправил стили и использую в base.html шаблоны типа: main:about для навигации

--- shop/main/views.py
+++ shop/main/views.py
@@ -4,9 +4,15 @@ from django.shortcuts import render
 def index(request):
     context = {
         "title":"Главная страница",
-        "content":"Главная страница"
+        "content":"Home - магазин мебели"
     }
     return render(request, "main/index.html", context)
 
 def about(request):
-    return HttpResponse("About")
+    context = {
+        "title": "О нас",
+        "content": "О нас",
+        "text_on_page":"Очень интересное описание товара"
+    }
+    return render(request, "main/about.html", context)
+
--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -16,15 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from main import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index, name="index"),
-    path("about/", views.about, name="about"),
+    path("", include('main.urls', namespace='main')) # Подключаю маршруты из приложения main
 ]
 
 # Добавляем обработку статических файлов для режима разработки
