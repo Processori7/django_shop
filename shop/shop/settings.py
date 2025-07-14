5ae2ef59f0262ba2a9a899e9420afa2ed0622d6a
Igor Usenko <usenkoigor01@gmail.com>
2025-07-14 12:28:59 +0300

    Добавил шаблоны

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -118,11 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "static/" # Просто это url адрес, не влияет на расположение файлов
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+    BASE_DIR / "static", #BASE_DIR сама изначально ссылается на папку shop
+    BASE_DIR.parent / "static" #BASE_DIR сама изначально ссылается на папку shop
+] # Расположение статических файлов
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from main import views
 
 urlpatterns = [
@@ -24,3 +26,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
 ]
+
+# Добавляем обработку статических файлов для режима разработки
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
