676cb59a234b091b41fe6aa32033b04b787d16ca
Igor Usenko <usenkoigor01@gmail.com>
2025-07-16 12:12:39 +0300

    Все переименовал

--- shop/goods/models.py
+++ shop/goods/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Categories(models.Model):
     # Поле ID создаётся автоматически
-    name = models.CharField(max_length=150, unique=True)
-    slug = models.SlugField(max_length=300, unique=True, blank=True, null=True)  # blank=True - поле может быть пустым, null=True - связано с blank
+    name = models.CharField(max_length=150, unique=True, verbose_name="Название")
+    slug = models.SlugField(max_length=300, unique=True, blank=True, null=True, verbose_name="URL")  # blank=True - поле может быть пустым, null=True - связано с blank
     
     class Meta:
         db_table = 'category'
