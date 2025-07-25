073393162dcab3c986c239c9afee238b7c2cf639
Igor Usenko <usenkoigor01@gmail.com>
2025-07-25 11:49:57 +0300

    Переписал логику для пагинации, добавил фильтры для товаров с сохранением

--- shop/goods/templatetags/goods_tags.py
+++ shop/goods/templatetags/goods_tags.py
@@ -2,6 +2,8 @@
 from goods.models import Products, Categories
 from django import template
 
+from django.utils.http import urlencode
+
 register = template.Library()  # должно быть template.Library()
 
 # Простой тег для получения всех категорий
@@ -13,3 +15,9 @@ def tag_categories():
 # @register.simple_tag()
 # def tag_products():
 #     return Products.objects.all()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+   query = context['request'].GET.dict()
+   query.update(kwargs)
+   return urlencode(query)
--- shop/goods/views.py
+++ shop/goods/views.py
@@ -3,7 +3,11 @@ from goods.models import Products, Categories
 from django.core.paginator import Paginator
 from django.http import Http404
 
-def catalog(request, category_slug='all', page=1):
+def catalog(request, category_slug='all'):
+
+    page = request.GET.get('page',1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
 
     if category_slug == 'all':
         goods = Products.objects.all()
@@ -13,9 +17,16 @@ def catalog(request, category_slug='all', page=1):
         # Фильтруем товары по категории
         goods = Products.objects.filter(category=category)
 
+    if on_sale:
+        # __gt - означает >
+        goods = goods.filter(discount__gt=0) 
+
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by)
+
     # Добавляю пагинацию
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context = {
         'title': 'Home - Каталог',
