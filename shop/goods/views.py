c6c5685289fa1cae3784b9c888fd0e8e15bcf225
Igor Usenko <usenkoigor01@gmail.com>
2025-07-28 13:02:17 +0300

    Добавил поиск по id и поиск по названию

--- shop/goods/views.py
+++ shop/goods/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from goods.models import Products, Categories
 from django.core.paginator import Paginator
 from django.http import Http404
+from goods.utils import q_search
 
-def catalog(request, category_slug='all'):
+
+def catalog(request, category_slug=None):
 
     page = request.GET.get('page',1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
     if category_slug == 'all':
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         # Находим категорию по slug
         category = get_list_or_404(Categories, slug=category_slug)
