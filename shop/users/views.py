d8d0bda1b95b3d0d63234c0efd622623659d615d
Igor Usenko <usenkoigor01@gmail.com>
2025-08-05 12:21:39 +0300

    Продолжаю работать с пользователями и формами, добавил авторизацию

--- shop/users/views.py
+++ shop/users/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+
+from users.forms import UserLoginForm
 
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            # Получаем данные из валидной формы
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            
+            # Аутентифицируем пользователя
+            user = auth.authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                # Если пользователь найден и активен
+                if user.is_active:
+                    auth.login(request, user)
+                    return HttpResponseRedirect(reverse('main:index'))
+                else:
+                    form.add_error(None, "Аккаунт отключен")
+            else:
+                form.add_error(None, "Неверное имя пользователя или пароль")
+    else:
+        form = UserLoginForm()
+
     context = {
-        'title': 'Home - Авторизация'
+        'title': 'Home - Авторизация',
+        'form': form
     }
     return render(request, 'users/login.html', context)
 
@@ -23,4 +51,5 @@ def profile(request):
 
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('main:index'))
